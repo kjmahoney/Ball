d96352b89c22a5a8482dddc4a2b0e11a9c85c8aa
Kevin Mahoney <kmahoney@theatlantic.com>
2018-03-26 17:33:29 -0400

    render ball and add crude x axis collision

--- public/app.js
+++ public/app.js
@@ -1,19 +1,47 @@
-console.log('hello');
+const THREE = require('three');
+const scene = new THREE.Scene();
 
-const Physics = require('physicsjs');
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const world = Physics();
+const renderer = new THREE.WebGLRenderer({canvas: document.getElementById('field')});
 
-var ball = Physics.body('circle', {
-    x: 50, // x-coordinate
-    y: 30, // y-coordinate
-    vx: 0.0, // velocity in x-direction
-    vy: 0.0, // velocity in y-direction
-    radius: 20
-});
-console.log(ball);
-const handleOrientation = (event) => {
-  //
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+			var cube = new THREE.Mesh( geometry, material );
+			scene.add( cube );
+
+camera.position.z = 5
+
+let velocity = 0.1;
+
+function animate() {
+	requestAnimationFrame( animate );
+  cube.rotation.x += 0.1
+  cube.rotation.y += 0.1
+
+  let widthHalf = (window.innerWidth / 2)
+  let heightHalf = (window.innerHeight / 2)
+
+  let cubeX = cube.position.x
+  let cubeY = cube.position.y
+
+  let cubeXscreen = (cubeX * widthHalf) + widthHalf
+
+  if (cubeXscreen > (window.innerWidth * 2)) {
+    velocity = -velocity;
+  }
+
+  if (cubeXscreen < (-window.innerWidth * 2)) {
+    velocity = -velocity;
+  }
+
+  cube.position.x += velocity
+
+	renderer.render( scene, camera );
 }
 
-window.addEventListener("deviceorientation", handleOrientation, true);
+animate();
