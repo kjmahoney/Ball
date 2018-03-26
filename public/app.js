b05de785353f8cdbc77deea0f2b9f285f90dce71
Kevin Mahoney <kmahoney@theatlantic.com>
2018-03-26 12:21:35 -0400

    add physicsjs

--- public/app.js
+++ public/app.js
@@ -1,7 +1,19 @@
 console.log('hello');
 
+const Physics = require('physicsjs');
+
+const world = Physics();
+
+var ball = Physics.body('circle', {
+    x: 50, // x-coordinate
+    y: 30, // y-coordinate
+    vx: 0.0, // velocity in x-direction
+    vy: 0.0, // velocity in y-direction
+    radius: 20
+});
+console.log(ball);
 const handleOrientation = (event) => {
-  console.log(event.alpha);
+  //
 }
 
 window.addEventListener("deviceorientation", handleOrientation, true);
