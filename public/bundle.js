8e6eb55d125bdcc3efb2f4eb3b3f963a471e4139
Kevin Mahoney <kjmahoney13@gmail.com>
2018-04-01 22:12:19 -0400

    push bundle

--- public/bundle.js
+++ public/bundle.js
@@ -1,39 +1,130 @@
 (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 const ball = document.getElementById('ball')
 const field = document.getElementById('field')
-ball.style.width = "25px"
-ball.style.height = "25px"
+const timer = document.getElementById('timer')
+const touches = document.getElementById('touches')
+
+const handleRotation = require('./rotation')
+const handleVelocity = require('./velocity')
+
+////
+//SETTINGS
+////
+ball.style.width = "50px";
+ball.style.height = "50px";
 ball.style.left = "50%"
 ball.style.top = "50%"
+
+let rotation = 1
+let xVelocity = 0
+let yVelocity = 0
+
+let touchesCounter = 0
+let timeCounter = 0
+
+const handleTimer = () => {
+	timeCounter	+=1
+	timer.innerHTML = timeCounter;
+}
+setInterval(handleTimer, 1000);
 
 handleOrientation = (event) => {
 	let yTilt = event.beta
 	let xTilt = event.gamma
 
-	let yIncrement = (yTilt / 10)
-	let xIncrement = (xTilt / 10)
+	//velocity changes in response to tilting device
+	////
+	xVelocity = handleVelocity.X(xTilt, xVelocity)
+	yVelocity = handleVelocity.Y(yTilt, yVelocity)
 
+	console.log(screen.width);
+
+	//changes in response to boundary collision
+	////
+	console.log(parseInt(ball.style.left) + parseInt(ball.style.width))
 	if ((parseInt(ball.style.left) + parseInt(ball.style.width)) >= screen.width) {
-		xIncrement = -xIncrement
+		xVelocity = -xVelocity
+		touchesCounter += 1
 	}
 
 	if (parseInt(ball.style.left) <= 0) {
-		console.log("COLLIDE");
-		ball.style.left = 0.1;
+		xVelocity = Math.abs(xVelocity)
+		touchesCounter += 1
 	}
 
-	if ((parseInt(ball.style.top) + parseInt(ball.style.height)) >= screen.height) {
-		yIncrement = -yIncrement
+	if ((parseInt(ball.style.top) + parseInt(ball.style.height)) >= window.innerHeight) {
+		yVelocity = -yVelocity
+		touchesCounter += 1
 	}
 
-	ball.style.top = parseInt(ball.style.top) + yIncrement + "px"
-	ball.style.left = parseInt(ball.style.left) + xIncrement + "px"
+	if (parseInt(ball.style.top) <= 0) {
+		yVelocity = Math.abs(yVelocity)
+		touchesCounter += 1
+	}
 
-	console.log(ball.style.left);
-	console.log(screen.height);
+	//animate the ball rotation
+	////
+	rotation = handleRotation.rotate(xVelocity, yVelocity, rotation);
+
+	//applies calculated changes
+	////
+	ball.style.top = parseInt(ball.style.top) + yVelocity + "px"
+	ball.style.left = parseInt(ball.style.left) + xVelocity + "px"
+	ball.style.webkitTransform = `rotate(${rotation}deg)`;
+	touches.innerHTML = touchesCounter
 }
 
-
 window.addEventListener("deviceorientation", handleOrientation, true);
+
+},{"./rotation":2,"./velocity":3}],2:[function(require,module,exports){
+module.exports = {
+  rotate: (xVelocity, yVelocity, rotation) =>  {
+    if (xVelocity == 0 && yVelocity == 0) {
+  		return rotation += 0
+  	}
+  	else if (xVelocity < 0) {
+  		return rotation -= 10
+  	}
+  	else {
+  		return rotation += 10
+  	}
+  }
+}
+
+},{}],3:[function(require,module,exports){
+module.exports = {
+
+  X: (xTilt, xVelocity) => {
+    let xIncrement = (.05 + (Math.abs(xTilt) / 10000))
+
+    if (xTilt >= 10) {
+      return xVelocity += xIncrement
+    }
+
+    else if (xTilt <= 0) {
+      return xVelocity -= xIncrement
+    }
+
+    else {
+      return xVelocity
+    }
+  },
+
+  Y: (yTilt, yVelocity) => {
+    let yIncrement = (.05 + (Math.abs(yTilt) / 10000))
+
+    if (yTilt >= 45) {
+      return yVelocity += yIncrement
+    }
+
+    else if (yTilt <= 35) {
+      return yVelocity -= yIncrement
+    }
+
+    else {
+      return yVelocity
+    }
+  }
+}
 
 },{}]},{},[1]);
